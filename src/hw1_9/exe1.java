c27c8212d2aa678d83d131d780d653a5f17fc9d2
DoctorSB <ikamil522@gmail.com>
2020-11-08 13:42:52 +0300

    Initial commit

---
+++ src/hw1_9/exe1.java
@@ -0,0 +1,14 @@
+package hw1_9;
+
+import java.util.Scanner;
+public class exe1 {
+        static int f(int a) {
+            return a / 10 + a % 10;
+        }
+
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+            int b = sc.nextInt();
+            System.out.println(f(b));
+        }
+    }
